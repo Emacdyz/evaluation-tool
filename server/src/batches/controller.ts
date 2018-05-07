7453216035054d5223e73e6bf4a446293024776a
Emacdyz <emeline.capdupuy@gmail.com>
2018-05-07 16:13:46 +0200

    revised entities of batches

---
+++ server/src/batches/controller.ts
@@ -0,0 +1,1 @@
+//src/batches/controller.ts
--- server/src/batches/entities.ts
+++ server/src/batches/entities.ts
@@ -1,5 +1,6 @@
 //src/batches/entities.ts
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+import {IsDateString} from "class-validator";
 import { BaseEntity } from 'typeorm/repository/BaseEntity'
 
 @Entity()
@@ -11,10 +12,12 @@ export class Batches extends BaseEntity {
     @Column('integer', {nullable: true})
     batchNb: number
 
+    @IsDateString()
     @Column('date', {nullable: true})
-    startDate: string 
+    startDate: Date 
 
+    @IsDateString()
     @Column('date', {nullable: true})
-    endDate: string 
+    endDate: Date
 
 }
